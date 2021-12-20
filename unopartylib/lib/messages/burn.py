d511c825ccf0e2febc7e9d1c7028f981922a167d
Andrew <95168952+terhnt@users.noreply.github.com>
2021-12-20 22:00:36 +1100

    fix testnet/regtest devfund

--- unopartylib/lib/messages/burn.py
+++ unopartylib/lib/messages/burn.py
@@ -105,9 +105,10 @@ def parse (db, tx, MAINNET_BURNS, message=None):
             # Credit source address with earned XUP.
             util.credit(db, tx['source'], config.XCP, earned, action='burn', event=tx['tx_hash'])
             
-            # If devfund activated - additional % goes to dev fund
-            if config.DEV_FUND == True:
-                util.credit(db, config.DEV_FUND_ADDR, config.XCP, int(math.ceil(earned * config.DEV_FUND_PERCENT)), action='burn', event=line['tx_hash'])
+            if config.DEV_FUND and config.TESTNET:
+                util.credit(db, config.DEV_FUND_ADDR_TESTNET, config.XCP, int(math.ceil(earned * config.DEV_FUND_PERCENT)), action='burn', event=line['tx_hash'])
+            elif config.DEV_FUND and config.TESTNET:
+                util.credit(db, config.DEV_FUND_ADDR_REGTEST, config.XCP, int(math.ceil(earned * config.DEV_FUND_PERCENT)), action='burn', event=line['tx_hash'])
         else:
             burned = 0
             earned = 0
@@ -127,7 +128,7 @@ def parse (db, tx, MAINNET_BURNS, message=None):
 
         util.credit(db, line['source'], config.XCP, int(line['earned']), action='burn', event=line['tx_hash'])
         
-        if config.DEV_FUND == True:
+        if config.DEV_FUND:
             util.credit(db, config.DEV_FUND_ADDR, config.XCP, int(math.ceil(line['earned'] * config.DEV_FUND_PERCENT)), action='burn', event=line['tx_hash'])
 
         tx_index = tx['tx_index']
