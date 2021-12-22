0476bef1d6e85b8e52feddee4af2358b53834ee5
Andrew <95168952+terhnt@users.noreply.github.com>
2021-12-22 17:51:35 +1100

    Remove 'P2SH_ADDR, P2WPKH_ADDR' from test scenario

--- unopartylib/test/fixtures/scenarios.py
+++ unopartylib/test/fixtures/scenarios.py
@@ -27,7 +27,7 @@ Here's a list of unit tests that will fail and need to be updated:
 - util.get_balance
 """
 
-from .params import ADDR, MULTISIGADDR, P2SH_ADDR, P2WPKH_ADDR, DEFAULT_PARAMS as DP
+from .params import ADDR, MULTISIGADDR, DEFAULT_PARAMS as DP
 
 UNITTEST_FIXTURE = [
     ['burn', (ADDR[0], DP['burn_quantity']), {'encoding': 'multisig'}],  # 310000
